e06d1e64f70491bcd971177384acb38eb636d194
arif-youknow <inbox.arifkhan@gmail.com>
2025-07-24 02:17:54 +0600

    basic setup done

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+static const Color brandColor = Color(0xFF84C000);
+static const Color inputFieldcolor = Color(0xFFF7FFEF);
+static  const Color black = Colors.black;
+static const Color white = Colors.white;
+
+
+}
